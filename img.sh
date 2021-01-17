1feaaaa4dc3dcb65e5f75dba90c9fc66dd39d262
Felipe sousa <71620144+phelipe06@users.noreply.github.com>
2021-01-17 13:20:44 -0300

    Add files via upload

---
+++ img.sh
@@ -0,0 +1,35 @@
+##!/bash/bin
+#foto
+
+white="\033[1;37m"
+purple="\033[0;35m"
+grey="\033[0;37m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+echo
+echo
+echo
+
+  echo -e " $green  imagem movida para a área de downloads$nc"
+echo -e " $blue                        @phelipe_011$nc❤"
+echo -e " $blue                        @_clar4m_$nc❤"
+  echo
+  echo
+  echo -e " $white ┏┓$nc$red┏┓┏━┳┳┳━┓$nc$white┏┳┳━┳┳┓$nc "
+  echo -e " $white ┃┃$nc$red┃┗┫┃┃┃┃━┫$nc$white┣┓┃┃┃┃┃$nc "
+  echo -e " $white ┗┛$nc$red┗━┻━┻━┻━┛$nc$white┗━┻━┻━┛$nc "
+  echo                                                                                       echo                                                                                 cp IMG_20201205_154439_002.jpg /data/data/com.termux/files/home/storage/downloads
+         echo
+       echo
+      echo
+    echo
+  echo
+echo
